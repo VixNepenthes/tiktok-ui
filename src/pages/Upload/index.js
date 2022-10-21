74d464f6395a095bd0c876485c8a7159e90fcee6
VixNepenthes <viht61220@gmail.com>
2022-10-21 21:56:44 +0700

    63

--- src/pages/Upload/index.js
+++ src/pages/Upload/index.js
@@ -1,9 +1,6 @@
-import Header from '~/components/Layout/DefaultLayout/Header';
-
 function Upload() {
     return (
         <div>
-            <Header />
             <h2>Upload page </h2>
         </div>
     );
